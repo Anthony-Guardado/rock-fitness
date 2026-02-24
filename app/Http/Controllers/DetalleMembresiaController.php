aad6c7e771d91dcc07f5610d7f231628565b9f0f
zarck <zarck430@gmail.com>
2026-02-23 18:55:06 -0600

    Para vincular merge

--- app/Http/Controllers/DetalleMembresiaController.php
+++ app/Http/Controllers/DetalleMembresiaController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class MetodoPagoController extends Controller
+class DetalleMembresiaController extends Controller
 {
     /**
      * Display a listing of the resource.
